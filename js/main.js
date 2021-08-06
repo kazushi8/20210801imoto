6fcbbe1baff98cd9eade9517ad01b8a097adbdf1
kazushi8 <kazushi88kazushi@gmail.com>
2021-08-06 19:05:34 +0900

    修正

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
-const target = document.getElementById("menu");
-target.addEventListener('click', () => {
-  target.classList.toggle('open');
-  const nav = document.getElementById("nav");
-  nav.classList.toggle('in');
-});
+(function($) {
+  var $nav   = $('#navArea');
+  var $btn   = $('.toggle_btn');
+  var $mask  = $('#mask');
+  var open   = 'open'; // class
+  // menu open close
+  $btn.on( 'click', function() {
+    if ( ! $nav.hasClass( open ) ) {
+      $nav.addClass( open );
+    } else {
+      $nav.removeClass( open );
+    }
+  });
+  // mask close
+  $mask.on('click', function() {
+    $nav.removeClass( open );
+  });
+})(jQuery);
